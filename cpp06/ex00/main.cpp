29b041c2fafbdb0847173270fe9bb8f3ec2564a6
Emma DE BANVILLE <ede-banv@student.42.fr>
2021-06-03 17:41:22 +0200

    im full of shit

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -1,6 +1,71 @@
 #include <iostream>
+#include <string>
 
-int	main(int ac, char** av)
+void	show_char(const std::string& arg)
 {
-	//find fct o do conversions?
+
+}
+
+void	show_int(const std::string& arg)
+{
+
+}
+
+void	show_float(const std::string& arg)
+{
+
+}
+
+void	show_double(const std::string& arg)
+{
+
+}
+
+int		main(int ac, char** av)
+{
+	if (ac != 2)
+	{
+		std::cout << "Wrong number of arguments.\n";
+	}
+	else
+	{
+		std::string arg = static_cast<std::string>(av[1]);
+		std::cout << "char: ";
+		try
+		{
+
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+		std::cout << "int: ";
+		try
+		{
+
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+		std::cout << "float: ";
+		try
+		{
+
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+		std::cout << "double: ";
+		try
+		{
+
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+	}
+
 }
