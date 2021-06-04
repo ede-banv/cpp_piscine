d6f806f3352b8894242c0976c78463fdc21b08af
Emma DE BANVILLE <ede-banv@student.42.fr>
2021-06-04 18:30:00 +0200

    chu kon

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
 #include <string>
 
-void	show_char(const std::string& arg)
+void	show_char(const double d)
 {
-
+	(void)d;
+	std::cout << "char: ";
 }
 
-void	show_int(const std::string& arg)
+void	show_int(const double d)
 {
-
+	std::cout << "int: ";
+	long int	i = static_cast<long int>(d);
+	if (i < INT_MIN || i > INT_MAX)
+		std::cout << "impossible\n";
+	else
+		std::cout << i << std::endl;
 }
 
-void	show_float(const std::string& arg)
+void	show_float(const double d)
 {
-
+	std::cout << "float: ";
+	float	f = static_cast<float>(d);
+	std::cout << f << "f" << std::endl;
 }
 
-void	show_double(const std::string& arg)
+void	show_double(const double d)
 {
-
+	std::cout << "double: " << d << std::endl;
 }
 
 int		main(int ac, char** av)
@@ -26,46 +34,15 @@ int		main(int ac, char** av)
 	if (ac != 2)
 	{
 		std::cout << "Wrong number of arguments.\n";
+		return (1);
 	}
 	else
 	{
 		std::string arg = static_cast<std::string>(av[1]);
-		std::cout << "char: ";
-		try
-		{
-
-		}
-		catch(const std::exception& e)
-		{
-			std::cerr << e.what() << '\n';
-		}
-		std::cout << "int: ";
-		try
-		{
-
-		}
-		catch(const std::exception& e)
-		{
-			std::cerr << e.what() << '\n';
-		}
-		std::cout << "float: ";
-		try
-		{
-
-		}
-		catch(const std::exception& e)
-		{
-			std::cerr << e.what() << '\n';
-		}
-		std::cout << "double: ";
-		try
-		{
-
-		}
-		catch(const std::exception& e)
-		{
-			std::cerr << e.what() << '\n';
-		}
+		double		d = std::stod(arg);
+		show_int(d);
+		show_float(d);
+		show_double(d);
 	}
-
+	return (0);
 }
