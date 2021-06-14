e2e2ddd0c465233f4abb0c69806d06d18d3b14bd
Emma DE BANVILLE <ede-banv@student.42.fr>
2021-06-14 17:07:33 +0200

    ex00 main added to

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -5,15 +5,27 @@ int main()
 {
 	int a = 2;
 	int b = 3;
+	int e = 3;
+
+	std::cout << "Before swap:\n" << "a = " << a << ", b = " << b << ", e = " << e << std::endl << std::endl;
 	::swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "After swap:\n" << "a = " << a << ", b = " << b << ", e = " << e << std::endl << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::cout << "min( a, c ) = " << ::min( a, e ) << std::endl;
+	std::cout << "max( a, c ) = " << ::max( a, e ) << std::endl;
+	std::cout<< std::endl << std::endl;
+
 	std::string c = "chaine1";
 	std::string d = "chaine2";
+	std::string f = "chaine2";
+
+	std::cout << "Before swap:\n" << "c = " << c << ", d = " << d << ", f = " << f << std::endl << std::endl;
 	::swap(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "After swap:\n" << "c = " << c << ", d = " << d << ", f = " << f << std::endl << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::cout << "min( c, f ) = " << ::min( c, f ) << std::endl;
+	std::cout << "max( c, f ) = " << ::max( c, f ) << std::endl;
 	return (0);
 }
