5f5ed9699de7cb5a3d7c30593641f607425c0b9e
Emma DE BANVILLE <ede-banv@student.42.fr>
2021-06-17 17:57:41 +0200

    deleted cpp, done class inheritance. to do: do iteration

--- cpp08/ex02/Mutantstack.hpp
+++ cpp08/ex02/Mutantstack.hpp
@@ -1,14 +1,17 @@
 #ifndef __MUTANTSTACK__
 # define __MUTANTSTACK__
 
-class Mutantstack
+# include <stack>
+
+template<typename T>
+class Mutantstack: public std::stack
 {
 	public:
-		Mutantstack();
-		Mutantstack(const Mutantstack& copy);
-		virtual ~Mutantstack();
+		Mutantstack(): std::stack<T>() {}
+		Mutantstack(const Mutantstack& copy): std::stack<T>(copy) {}
+		~Mutantstack() {}
 
-		Mutantstack&	operator=(const Mutantstack& rhs);
+		Mutantstack&	operator=(const Mutantstack& rhs) { std::stack<T>::operator=(rhs); }
 	private:
 		/*args*/
 };
